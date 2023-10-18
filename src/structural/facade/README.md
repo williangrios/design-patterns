d0695f19cceb9707439ee9e1aecba7b3ae6d2cd6
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-10-18 19:58:01 -0300

    feat: add facade pattern

---
+++ src/structural/facade/README.md
@@ -0,0 +1,1 @@
+# é um padrão de projeto estrutural que tem a inteção de fornecer uma interface unificada para um conjunto de interfaces em um subsistema. Façade define uma interface de nível mais alto que torna o subsistema mais fácil de ser usado
---
+++ src/structural/facade/builder-facade.ts
@@ -0,0 +1,21 @@
+import { MainDishBuilder } from "../../creational/builder/example1/classes/main-dish-builder"
+
+export class BuilderFacade{
+    // essa é a fachada para a pasta creational/builder/example1/index.ts
+    private mainDishBuilder = new MainDishBuilder()
+    
+    makeMeal1(): void{
+        this.mainDishBuilder.makeMeal().makeDessert()
+        console.log(this.mainDishBuilder.getMeal())
+        this.mainDishBuilder.reset()
+        this.mainDishBuilder.makeMeal()
+        console.log(this.mainDishBuilder.getPrice())
+
+    }
+
+    makeMeal2(): void{
+        this.mainDishBuilder.reset()
+        const meal2 = this.mainDishBuilder.makeBeverage().getMeal()
+        console.log(meal2);
+    }
+}
