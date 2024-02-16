4e69cc5f27ac8ddf94bdb618d3b2dcfa11c276bb
Willian Goncalves Rios <williangrios@yahoo.com.br>
2024-02-16 06:56:56 -0300

    feat: add decorator example2

---
+++ src/structural/decorator/example2/index.ts
@@ -0,0 +1,40 @@
+interface Coffee {
+    cost():number;
+    description(): string
+}
+
+class SimpleCoffee implements Coffee {
+    public cost(): number {
+        return 10
+    }
+    public description(): string {
+        return "Simple Coffee"
+    }
+}
+
+abstract class CoffeeDecorator implements Coffee {
+    constructor(protected coffee: Coffee){
+
+    }
+    abstract cost(): number 
+    abstract description(): string
+}
+
+class MilkDecorator extends CoffeeDecorator{
+    constructor (coffee: Coffee){
+        super(coffee)
+    }
+    public cost(): number {
+        return this.coffee.cost() + 2
+    }
+    public description(): string {
+        return ` ${this.coffee.description()}, with milk`
+    }
+}
+
+// client code
+let coffee: Coffee = new SimpleCoffee()
+coffee = new MilkDecorator(coffee) // the coffe is being updating here
+
+console.log(`Cost: ${coffee.cost()}`)
+console.log(`Description: ${coffee.description()}`)
